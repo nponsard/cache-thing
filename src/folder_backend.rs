515741785789e61337782c68378461c9706558dd
Nils Ponsard <nils.ponsard@inria.fr>
2025-09-16 12:27:51 +0200

    feat: hash file name to avoid forbidden characters
    Signed-off-by: Nils Ponsard <nils.ponsard@inria.fr>

--- src/folder_backend.rs
+++ src/folder_backend.rs
@@ -1,7 +1,13 @@
 use log::trace;
+use sha2::{Digest, Sha256};
 use std::fs::{File, OpenOptions};
 
 use crate::storage_backend::StorageBackend;
+
+fn hash_file_name(key: &str) -> String {
+    let hash = Sha256::digest(key);
+    base16ct::lower::encode_string(&hash)
+}
 
 pub struct FolderBackend {
     base_path: std::path::PathBuf,
@@ -16,13 +22,15 @@ impl FolderBackend {
 impl StorageBackend for FolderBackend {
     type Error = std::io::Error;
     fn reader(&self, key: &str) -> Result<impl std::io::Read, Self::Error> {
-        let path = self.base_path.join(key);
+        let name = hash_file_name(key);
+        let path = self.base_path.join(name);
         let file = File::open(path)?;
         file.lock_shared()?;
         Ok(file)
     }
     fn writer(&self, key: &str) -> Result<impl std::io::Write, Self::Error> {
-        let path = self.base_path.join(key);
+        let name = hash_file_name(key);
+        let path = self.base_path.join(name);
         trace!("Writing to path {:?}", path);
         if let Some(parent) = path.parent() {
             trace!("Creating parent directory {:?}", parent);
@@ -42,7 +50,8 @@ impl StorageBackend for FolderBackend {
         Ok(Box::new(file))
     }
     fn exists(&self, key: &str) -> Result<bool, Self::Error> {
-        let path = self.base_path.join(key);
+        let name = hash_file_name(key);
+        let path = self.base_path.join(name);
         Ok(path.exists())
     }
 }
